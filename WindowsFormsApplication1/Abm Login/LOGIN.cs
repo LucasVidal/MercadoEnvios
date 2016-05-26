9539deb08a7605a6bc2701b8e4d03ffad606be5e
Lucas Vidal <lucasvidalutn@gmail.com>
2016-05-26 11:24:57 -0300

    Initial commit

---
+++ WindowsFormsApplication1/Abm Login/LOGIN.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using MercadoEnvio.Abm_Menu;
+using MercadoEnvio.Base_De_Datos;
+
+namespace MercadoCLass
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (Conexion.conectar())
+            {
+                DataTable users = new DataTable();
+                string cod = textBox1.Text;
+                string pass = textBox2.Text;
+                //string cadena = "select usuario as user_name,clave as contraseña ,loginfallidos as login_fallidos,IdRol as id_rol from Class.Usuario where clave=HASHBYTES('SHA2_256','" + pass.Trim() + "') and Usuario=('" + cod.Trim() + "')";
+                string cadena = "select IdUsuario,usuario as user_name,clave as contraseña ,loginfallidos as login_fallidos from Class.Usuario where clave=HASHBYTES('SHA2_256','" + pass.Trim() + "') and Usuario=('" + cod.Trim() + "')";
+                users = Conexion.LeerTabla(cadena);
+                if (users.Rows.Count == 0)
+                {
+                    MessageBox.Show("Error al ingresar el Usuario o Contraseña");
+                    cadena = "update Class.Usuario set LoginFallidos=LoginFallidos+1 where Usuario=upper('" + cod.Trim() + "')";
+                    int resultado = Conexion.EjecutarComando(cadena);
+                }
+                else
+                {
+                    foreach (DataRow fila in users.Rows)
+                    {
+                        if (int.Parse((fila["login_fallidos"].ToString())) < 3)
+                        {
+                            this.Hide();
+                            MessageBox.Show("Bienvenido " + fila["user_name"].ToString());
+                            AbmMenu menu = new AbmMenu();
+
+                            //menu.CargarMenu(fila["IdUsuario"].ToString());
+                            menu.CargarMenu(fila["IdUsuario"].ToString(), fila["user_name"].ToString());
+                            menu.Show();
+
+                        }
+                        else
+                        {
+                            MessageBox.Show("El Usuario" + fila["user_name"].ToString() + " está Bloqueado");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error al realizar conexion con el motor de Base de Datos");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido, ha ingresado como invitado");
+            AbmMenu menu = new AbmMenu();
+            menu.CargarMenu("2","");
+            menu.Show();
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Abm Menu/AbmMenu.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MercadoEnvio.Base_De_Datos;
+using WindowsFormsApplication1.ABM_Usuario;
+/*
+using MercadoEnvio.Abm_Ciudad;
+using MercadoEnvio.Registro_Llegada_Destino;
+using MercadoEnvio.Generacion_Viaje;
+using MercadoEnvio.Abm_Ruta;
+using MercadoEnvio.Compra;
+using MercadoEnvio.Consulta_Millas;
+using MercadoEnvio.Abm_Aeronave;
+using MercadoEnvio.Listado_Estadistico;
+using MercadoEnvio.Abm_Rol;
+using MercadoEnvio.Canje_Millas;
+using MercadoEnvio.Registro_de_Usuario;
+*/
+
+
+namespace MercadoEnvio.Abm_Menu
+{
+    public partial class AbmMenu : Form
+    {
+        public AbmMenu()
+        {
+            InitializeComponent();
+            //
+        }
+
+        public void CargarMenu(string idusuario,string usuario)
+        {
+            textBox1.Text = usuario;
+            textBox1.Enabled = false;
+            DataTable rolUsado = new DataTable();
+            string cadena = "select ru.IdRol,r.nombre from Class.RolUsuario ru join Class.Rol r on ru.idrol=r.idrol where ru.IdUsuario='" + idusuario + "' order by Orden";
+            rolUsado = Conexion.LeerTabla(cadena);
+            
+            comboBox1.DisplayMember = "nombre";
+            comboBox1.ValueMember = "IdRol";
+            comboBox1.DataSource = rolUsado;
+            comboBox1.SelectedIndex = 0;
+            //
+            CargarMenu2();
+            
+        }
+        public void CargarMenu2()
+        {
+            DataTable funcionalidades = new DataTable();
+            //cadena = "select RolFuncionalidad.idFuncionalidad as id_func,nombre from Class.RolFuncionalidad join Class.Funcionalidad on RolFuncionalidad.idFuncionalidad=funcionalidad.idFuncionalidad where RolFuncionalidad.idRol=" + usuario + " and TienePermisos=1";            
+            string cadena = "select RolFuncionalidad.idFuncionalidad as id_func,nombre from Class.RolFuncionalidad join Class.Funcionalidad on RolFuncionalidad.idFuncionalidad=funcionalidad.idFuncionalidad where RolFuncionalidad.idRol=" + comboBox1.SelectedValue.ToString().Trim() + " and TienePermisos=1";
+            funcionalidades = Conexion.LeerTabla(cadena);
+            if (funcionalidades.Rows.Count != 0)
+            {
+                foreach (DataRow fila in funcionalidades.Rows)
+                {
+                    int puntomenu = int.Parse(fila["id_func"].ToString());
+                    switch (puntomenu)
+                    {
+                        case 1:
+                            button1.Enabled = true;
+                            break;
+                        case 2:
+                            button2.Enabled = true;
+                            break;
+                        case 3:
+                            button3.Enabled = true;
+                            break;
+                        case 4:
+                            button4.Enabled = true;
+                            break;
+                        case 5:
+                            button5.Enabled = true;
+                            break;
+                        case 6:
+                            button6.Enabled = true;
+                            break;
+                        case 7:
+                            button7.Enabled = true;
+                            break;
+                        case 8:
+                            button8.Enabled = true;
+                            break;
+                        case 9:
+                            button9.Enabled = true;
+                            break;
+                        case 10:
+                            button10.Enabled = true;
+                            break;
+                    }
+
+                }
+            }
+            else
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = false;
+                button7.Enabled = false;
+                button8.Enabled = false;
+                button9.Enabled = false;
+                button10.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AbmMenu_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        }
+
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarMenu2();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
